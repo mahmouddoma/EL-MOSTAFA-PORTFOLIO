57e2ee114bd62bfc6a61a99f93f626eb2fe0fca0
mahmouddoma <devdoma2002@gmail.com>
2026-04-22 10:21:00 +0200

    useAdminMockFallback = flase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiRootUrl: 'https://elmostafaportfolio.somee.com/api',
   apiV1BaseUrl: 'https://elmostafaportfolio.somee.com/api/v1',
-  useAdminMockFallback: true,
+  useAdminMockFallback: false,
 };
